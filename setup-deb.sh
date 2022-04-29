99347e3b56cb16b64a4626964f5ba4cb25f61a08
Luka Čabraja <luka@cabraja.eu>
2022-04-29 12:29:06 +0200

    Update setup-deb.sh

--- setup-deb.sh
+++ setup-deb.sh
@@ -1,4 +1,5 @@
 # Essentials
+sudo apt install -y apt-transport-https ca-certificates curl gnupg
 
 # 1Password + 1Password CLI
 # https://support.1password.com/install-linux/
@@ -9,7 +10,8 @@ sudo mkdir -p /etc/debsig/policies/AC2D62742012EA22/
 curl -sS https://downloads.1password.com/linux/debian/debsig/1password.pol | sudo tee /etc/debsig/policies/AC2D62742012EA22/1password.pol
 sudo mkdir -p /usr/share/debsig/keyrings/AC2D62742012EA22
 curl -sS https://downloads.1password.com/linux/keys/1password.asc | sudo gpg --dearmor --output /usr/share/debsig/keyrings/AC2D62742012EA22/debsig.gpg
-sudo apt update && sudo apt install -y 1password 1password-cli
+sudo apt update
+sudo apt install -y 1password 1password-cli
 
 # Tailscale
 # https://tailscale.com/download
@@ -17,18 +19,19 @@ curl -fsSL https://tailscale.com/install.sh | sh
 
 # Docker
 # https://docs.docker.com/engine/install/ubuntu/
-sudo apt-get update
-sudo apt-get install -y ca-certificates curl gnupg lsb-release
+sudo apt update
+sudo apt install -y ca-certificates curl gnupg lsb-release
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu$(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update && sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+sudo apt update
+sudo apt install -y docker-ce docker-ce-cli containerd.io
 
 # Docker Compose V2
 # https://docs.docker.com/compose/cli-command/
 COMPOSE_LOCATION=/usr/local/lib/docker/cli-plugins
-mkdir -p $COMPOSE_LOCATION
-curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $$COMPOSE_LOCATION/docker-compose
-chmod +x $COMPOSE_LOCATION/docker-compose
+sudo mkdir -p $COMPOSE_LOCATION
+sudo curl -SL https://github.com/docker/compose/releases/download/v2.2.3/docker-compose-linux-x86_64 -o $COMPOSE_LOCATION/docker-compose
+sudo chmod +x $COMPOSE_LOCATION/docker-compose
 
 # Compose switch
 # Compose Switch is a replacement to the Compose V1 docker-compose (python) executable. Compose switch translates the command line into Compose V2 docker compose and then runs the latter.
@@ -36,10 +39,11 @@ curl -fL https://raw.githubusercontent.com/docker/compose-switch/master/install_
 
 # Doppler
 # https://docs.doppler.com/docs/install-cli
-sudo apt-get update && sudo apt-get install -y apt-transport-https ca-certificates curl gnupg
+sudo apt update
 curl -sLf --retry 3 --tlsv1.2 --proto "=https" 'https://packages.doppler.com/public/cli/gpg.DE2A7741A397C129.key' | sudo apt-key add -
 echo "deb https://packages.doppler.com/public/cli/deb/debian any-version main" | sudo tee /etc/apt/sources.list.d/doppler-cli.list
-sudo apt-get update && sudo apt-get install doppler
+sudo apt update
+sudo apt install doppler
 
 # SDKs
 
